9c60fe7c2f05caf72d808448c18b1fd9f11f25da
Ikonicity Airban <ikonicityairban@gmail.com>
2023-05-28 00:26:35 +0100

    refactoring
    I have refactor most of the setup from full-stack to only backend setup

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,12 +5,9 @@ import {
   RenderLogin,
 } from "../controllers/auth.controller.js";
 import { studentDashboard } from "../controllers/dashboard.js";
-import passport from "../utils/passport.js";
+
 const router = Router();
-
 router.route("/login").get(RenderLogin).post(Login);
-
-router.route("/dashboard/student").get(studentDashboard);
 
 //export
 const authRouter = router;
